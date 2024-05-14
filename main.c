80193ec136f568f3df4de35bd008ac83c150da67
ibaimason <asier.sala01@estudiant.upf.edu>
2024-05-14 23:18:04 +0200

    nueva

--- main.c
+++ main.c
@@ -1,94 +1,124 @@
 #include "main.h"
 
 void inicializar_enemigos(){
-    Enemigo fantasma;
-    init_enemigo(&fantasma, "Fantasma de soldado", 100, 50, 20);
+    Enemigo fantasma1;
+    init_enemigo(&fantasma1, "Fantasma de soldado", 100, 50, 20);
+
+    Enemigo fantasma2;
+    init_enemigo(&fantasma2, "Fantasma de soldado", 100, 50, 20);
    
     Enemigo cofretrampa;
     init_enemigo(&cofretrampa, "Cofre Trampa", 225, 100, 200);
-    
+
+    //Enemigo enemigos_opcion1[] = {fantasma,cofretrampa};
+    //inicializar_opcion(enemigos_opcion1);
 
     Enemigo subordinadoelfo;
     init_enemigo(&subordinadoelfo, "Subordinado elfo", 125, 120, 80);
     
-
     Enemigo jefoelfe;
     init_enemigo(&jefoelfe, "Elfo Jefe", 400, 150, 300);
     
+    Enemigo tortuguita1;
+    init_enemigo(&tortuguita1, "Tortugas", 155, 210, 400);
 
-    Enemigo tortuga;
-    init_enemigo(&tortuga, "Tortuga", 205, 110, 500);
+    Enemigo tortuguita2;
+    init_enemigo(&tortuguita2, "Tortugas", 155, 210, 400);
     
-
+    Enemigo tortugon;
+    init_enemigo(&tortugon, "Tortuga", 205, 110, 500);
+    
     Enemigo miniblaze;
     init_enemigo(&miniblaze, "Secuaz de Blaze", 350, 120, 300);
     
-
     Enemigo blaze;
     init_enemigo(&blaze, "BLAZE", 1000, 400, 600);
-    
+
+    Enemigo enemigos[][2] = {{fantasma1,fantasma2},{cofretrampa},{subordinadoelfo,jefoelfe},{tortuguita1,tortuguita2},{tortugon},{miniblaze,blaze},{blaze}};
+    inicializar_opcion(enemigos);
 }
 
-void inicializar_decision(){
-    Decision cementerio;
-    init_decision(&cementerio,"Después de derrotar a todos los enemigos, te encuentras a la salida del cementerio. Estando todavía desconcertado, no te estás del todo situado en tu antigua vida, asi que no sabes que camino hay que tomar. Por donde deseas ir?", 3, "");
-    print(&cementerio);
+void inicializar_opcion(Enemigo enemigos[][2]){
+    Opcion opcion1_1; 
+    init_opcion(&opcion1_1, "Luchar", "Te encuentras con un enemigo poderoso:", enemigos[0], 2, "Has derrotado al enemigo.");
 
-    Decision bosque;
-    init_decision(&bosque, "Perdido dentro del bosque, te encuentras con unos carteles que te dan información sobre que ruta escoger. Una vez leídos, hacía donde decides partir?",3,"");
-    print(&bosque);
+    Opcion opcion1_2; 
+    init_opcion(&opcion1_2, "Luchar", "Te encuentras con un enemigo poderoso:", enemigos[1], 1, "Has derrotado al enemigo.");
 
-    Decision lago;
-    init_decision(&lago,"Contemplando de una vez el azul claro del agua del lago, observas al frente y ves que hay ciertos caminos que no están en muy buenas condiciones. Estando el camino partido en 3, hacia donde deseas partir para llegar al casitllo?",3,"");
-    print(&lago);
+    Opcion opcion1_3;
+    init_opcion(&opcion1_3,"Luchar","No te has encontrado con ningún enemigo",NULL,0,"Consigues llegar al bosque sin pelear con nadie");
+
+    Opcion opcion2_1; 
+    init_opcion(&opcion2_1, "Luchar", "Te encuentras con un enemigo poderoso:", enemigos[2], 2, "Has derrotado al enemigo.");
+
+    Opcion opcion2_2; 
+    init_opcion(&opcion2_2,"Luchar","No te has encontrado con ningún enemigo",NULL,0,"Consigues llegar al lago sin pelear con nadie");
+
+    Opcion opcion2_3; 
+    init_opcion(&opcion2_2,"Luchar","No te has encontrado con ningún enemigo",NULL,0,"Consigues llegar al lago sin pelear con nadie");
+
+    Opcion opcion3_1; 
+    init_opcion(&opcion3_1, "Luchar", "Te encuentras con un enemigo poderoso:", enemigos[3], 2, "Has derrotado al enemigo.");
+    
+    Opcion opcion3_2; 
+    init_opcion(&opcion3_2, "Luchar", "Te encuentras con un enemigo poderoso:", enemigos[4], 1, "Has derrotado al enemigo.");
+
+    Opcion opcion3_3;
+    init_opcion(&opcion3_3,"Luchar","No te has encontrado con ningún enemigo",NULL,0,"Consigues llegar al lago sin pelear con nadie");
+
+    Opcion opcion4_1; 
+    init_opcion(&opcion4_1, "Luchar", "Te encuentras con un enemigo poderoso:", enemigos[5], 2, "Has derrotado al boss final.");
+
+    Opcion opcion4_2; 
+    init_opcion(&opcion4_2, "Luchar", "Te encuentras con un enemigo poderoso:", enemigos[6], 1, "Has derrotado al boss final.");
+    
+    //print_opcion(&opcion1_1);
+    Opcion opciones[][3] = {{opcion1_1,opcion1_2,opcion1_3},{opcion2_1,opcion2_2,opcion2_3},{opcion3_1,opcion3_2,opcion3_3},{opcion4_1,opcion4_2}};
+    inicializar_decision(opciones);
 }
 
+void inicializar_decision(Opcion opciones[][3]){
+    Decision decision1;
+    init_decision(&decision1, "¿Qué quieres hacer?", opciones[0], 3);
 
+    Decision decision2;
+    init_decision(&decision2, "Así que, por donde deseas ir?", opciones[1], 3);
 
-void inicializar_opcion(){
-    Opcion cementerio1;
-    init_Opcion(&cementerio1,"Has decidido escoger el camino derecho","Este camino derecho te llevaba a la casa de tu viejo amigo RUFF, el cuál es herrero", , , "Llegas a la casa del viejo herrero y el viejo, no se puede creer que estés delante suya. Se piensa que todo es una mentira y está soñando despierto, pero nombras a “SIRA” y entonces sabe que eres real. Este decide ayudarte y acaba explicándote todo lo que ha sucedido en estos años. Recibes una katana, la cúal incrementará +20 el ataque y una armadura que te subirá en +10. Lo recoges todo y sigues adelante");
+    Decision decision3;
+    init_decision(&decision3, "¿Cuál camino vas a coger?", opciones[2], 2);
+
+    Decision decision4;
+    init_decision(&decision4,"Qué camino deseas tomar?", opciones[3], 2);
     
-    Opcion cementerio2;
-    init_Opcion(&cementerio2,"Has decidido escoger el camino izquierdo", "En el camino izquierdo contemplabas un cofre brillante que podria contener ciertos elementos curativos, de subida de defensa, mejora de ataques…", &cofretrampa, 1, "Llegas al cofre en medio del bosque y te encuentras en que es un cofre trampa, el cuál salta a atacarte” → Lo matas, saqueas el loot, el cual te dará 2 pociones de vida y 2 de escudo");
+    Decision decisiones[] = {decision1,decision2,decision3,decision4};
     
-    Opcion cementerio3;
-    init_Opcion(&cementerio3, "Has decidido ir por el camino central","Bien sabias que si decideias tirar recto por el camino central, llegarías al bosque más rápidamente escapando de todo posible enfrentamiento.", , ,"Asi que corres ràpidamente por el medio del camino, esquivando a todos los enemigos que vas observando. En tus pies encuentras 1 poción de vida y varios càdaveres de guerreros. Despúés de un tiempo, llegas al bosque");
-    
-    Opcion bosque1;
-    init_Opcion(&bosque, "Decides seguir el cártel de la derecha", "Si ibas por la derecha, te encontrarás con una tienda en la cuál podrias comprar ciertos objetos antes de  llegar a tu próximo destino", , , "Acabas llegando a la tienda donde decides adquirir una poción de vida");
-
-    Opcion bosque2;
-    init_Opcion(&bosque, "Decides ir por el cártel izquierdo" ,"Este cártel te decia que llegarías al lago por el camino más rápido de todos escapando de todo enemigo y de todo posible lugar de looteo ", , , "Como el cártel te informaba, has llegado al lago escapando de toda pelea posible y sigues con los mismos objetos que al principio");
-
-    Opcion bosque3;
-    init_Opcion(&bosque, "Decies ir por el cártel central", "Este camino central te indicaba que pelearías contra el elfo supremo y sus secuaces, y tu, sin miedp, has decidido pelear", &subordinadoelfo &elfosupremo, 2 1 , "La pelea contra este jefe es tuya y con ello te has llevado su gran armadura, la qual te ha otorgado +20 de defensa");
-    
-    Opcion lago1;
-    init_Opcion(&lago,"Pillas el camino derecho", " El camino de la derecha está más destrozado pero observas que al final del camino hay unas ruinas de un pequeño castillo destrozado en el cúal podrías encontrar ciertas mejoras para llegar al castillo final", , , "Has logrado llegar a las ruinas donde te has encontrado a la ESPADA SAGRADA, la cuál perteneció a grandes héroes històricos pasados. Esta te sumará +80 de ataque, asi que te equipas con ella y sigues hacia delante");
-
-    Opcion lago2;
-    init_Opcion(&lago,"Pillas el camino izquierdo", "El camino izquierdo aparenta vacio y sin enemigos espérandote, por lo que podrias coger ese camino para llegar más ràpido", &tortuga, 1 , "Una tortuga bandida te ha asaltado, logrando acabar con ella, has continuado tu viaje hasta el gran castillo");
-
-    Opcion lago3;
-    init_Opcion(&lago,"Pillas el camino central", "En el camino del medio se interpone un grupo de tortugas humanizadas, las cúales portan armas y tienen sed de robarte todo lo que tengas encima. Tu sin miedo, decides ir a por ellas", &tortuga, 2, "Logras acabar con las tortugas las cuáles te darán una poción de recuperación de vida y otra de maná" );
+    //printf("----------------------------------------------------\n");
+    //printf("\n Decisioneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeees:\n \n");
+    //print_decision(&decision1);
+    inicializar_escenario(decisiones);
 }
 
-void inicializar_escenario(){
-    Escena cementerio;
-    init_escena(&cementerio, "Cementerio de KINGSLAND", "El aire que se puede respirar es tenebroso y todo lo que te rodea son tumbas de tus compañeros soldados caídos en batalla al largo de estos años. Puedes contemplar que todo está más deteriorado por culpa de las continúas guerras, cosa que está provocando que los territorios se vayan malgastando");
+void inicializar_escenario(Decision decisiones[]){
+    // Crear el escenario
+    
+    Escenario escenario1;
+    init_escenario(&escenario1, "Escenario 1", "Acabas de aparecer en medio del cementerio de KINGSLAND, la ciudad que salvaste en el pasado, y desconcertado, no acabas de reconocer todo el terreno que te rodea. El aire es crudo y puedes sentir un aire de temor y oscuridad por las tierras que antes te pertenecian. Ante la sensación de inseguridad, ràpidamente, coges la espada que esta clavada en tu tumba, col·locada por “alguien” quien parece que pensaba que algun dia volverias al mundo real. Observas que se te acercan unos fantasmas de no muy buena espina, asi que podrías tomar diferentes caminos para salir de aquí. Recuerdas que si cogias el camino derecho, llegabas hacia la casa del viejo herrero, llamado “RUFF”, con el cuál tenias una buena amistad. Este podria ayudarte a resolver tus dudas y él porque has vuelto al mundo real. Por otra parte, en el camino izquierdo contemplas un cofre brillante que podria contener ciertos elementos curativos, de subida de defensa, mejora de ataques…Estos 2 caminos nombrados tienen una alta probabilidad de encontrarse con enemigos, en cambio, si decides tirar recto por el camino central, llegarías al bosque más rápidamente escapando de todo posible enfrentamiento", &decisiones[0], 1);
 
-    Escena bosque;
-    init_escena(&bosque, "El bosque maldito", "El bosque oscuro maldito de KINGSLAND, conocido por el hecho de que hay gente que ha logrado entrar pero no salir...El aire que se respira por esta zona es intenso y fuerte, los pulmones se te llenan de un oxigeno malvado y la gran cantidad de árboles provoca que no puedas contemplar nada de lo que haya dentro de este bosque", );
+    Escenario escenario2;
+    init_escenario(&escenario2, "Escenario 2", "Despúes de una larga caminata, llegas por fin al bosque oscuro de KINGSLAND. El bosque está tan arboleado que es imposible ver lo que hay detrás de cada árbol, todo parece un sitio sin salida. En la entrada de este, te encuentras unos carteles que te indican 3 caminos por los cúales poder avanzar. Solo oyes gritos de desesperación, espadas chocándose y el sondio del viento zarandeando las hojas que componen el gran bosque. El cartel del medio te dice que si sigues hacia adelante te encontrarás con el elfo supremo, el cuál te puede hacer conseguir una gran recompensa si es derrotado. El cartel de la izquierda, te dice que puedes coger este camino el cuál es el más rápido para llegar al lago, escapando de todo enemigo posible. Por la otra banda, el cartel derecho te informa de que una tienda te encontrarás en el caso de que este camino cojas, pudiendo lootear asi antes de pasar a la siguiente zona", &decisiones[1], 1);
 
-    Escena lago;
-    init_escena(&lago, "Lago del sueño","Después de una larga aventura, llegas a contemplar al fin el gran azul reflejado en el agua del lago de KINGSLAND. El agua del lago puede tranqulizar a todo ser que la contemple gracias a su gran luz brillante, todo la tranqulidad parece estar reflejada en este lago, porque sus alrededores están todos destrozados." );
+    Escenario escenario3;
+    init_Escenario(&escenario3,"Escenario 3","Ya superado el bosque, te encuentras con el gran sonido pacífico del lago. Toda maldad que hayas podido encontrar en las zonas anteriores se desvanece ante el gran paisaje que el lago magistral te hace contemplar. Los caminos están todos destrozados y ves a lo lejos diferentes tipos de animales marítimos humanizados, así como grandes tortugas. Estas, se encuentran en el camino del medio, específicamente 2, esperándote a que pases por ahí para intentar quitarte todo lo que tienes. Por la izquierda contemplas aparantemente un camino vacío y sin enemigos, aunque parece que ciertas batallas han sucedido alli hace poco. El camino derecho está en sus últimas condiciones, el peor de todos, pero al final de este puedes contemplar un pequeño castillo abandonado en el cuál podrias coger ciertas cosas antes de partir",&decisiones[2],1);
 
-    Escena castillo;
-    init_escena(&castillo, "El gran castillo de KINGSLAND","El final de tu largo recorrido prevalece en este territorio, el gran castillo. Nada más entrar, todos tus buenos antiguos recuerdos por las calles y rincones de este castillo llenan tu mente. Toda la felicidad que antes había en estas calles se ha arrebatado con el paso del tiempo y ahora simplemente hay alboroto y miedo.",  );
+    Escenario escenario4;
+    init_escenario(&escenario4, "Escenario 4", "Acabas de llegar al castillo de KINGSLAND, donde todo está destrozado y solo se oyen gritos de sufrimiento por las calles de tu villa. Arrebatado por la furia y la venganza, tu rabia empieza a incrementar exageradamente, y con ella, tus ganas de asesinar. Empiezas a correr acabando con la vida de todo malvado que se cruce en tu camino, en un visto y visto, has asesinado aproximadamente a más de 100 subordinados. Logras llegar, empapado de sangre de los enemigos, a la puerta del gran castillo, la cuál está bloqueda. En tu camino de la izquierda observas que llegarías dentro del castillo superando antes 2 grandes subordinados de BLAZE. Por la derecha, superarías ciertos obstàculos sin pelearte para llegar al centro dell castillo. Ambos, te van a llevar al final de tu aventura, donde la princesa SIRA se encuentra emprisionada", &decisiones[3], 1);
 
+    // Imprimir el escenario
+    //printf("----------------------------------------------------\n");
+    print_escenario(&escenario2);
 }
-int main(){
+
+int main() {
     // Crear una instancia de la estructura Personaje
     Personaje miPersonaje;
 
@@ -96,11 +126,11 @@ int main(){
     init_personaje(&miPersonaje);
 
     // Mostrar los datos del personaje configurado
+    printf("\nPensonaje: \n");
     print_personaje(&miPersonaje);
-
+    printf("----------------------------------------------------\n");
+    printf("\nEnemigos:\n \n");
     inicializar_enemigos();
-    inicializar_decision();
-    inicializar_escenario();
-    inicializar_opcion();
     return 0;
 }
+
